4a2e2e5376be41535e1316ac9c3a5c0ded547315
aleksromanov <kosmonavt-vavilon@mail.ru>
2022-05-18 02:54:41 +0700

    Создание структуры и разметки страницы ..

--- pages/index.js
+++ pages/index.js
@@ -2,56 +2,62 @@ import Link from "next/link";
 import logoPic from "./pictures/krk_logo.svg";
 import avaPic from "./pictures/ava1.png";
 import Image from 'next/image'
-import {Layout} from "antd";
+import {Form, Layout} from "antd";
+import Sider from "antd/lib/layout/Sider";
 
-const {Sider, Content} = Layout;
+
+const {Content} = Layout;
 
 const Index = () => {
 
-
     return (
-        <div className="container" id="container" style={{padding: "24px"}}>
+        <div className="container">
             <div className="header">
                 <div className="header-body">
                     <Image src={logoPic}/>
                     <div className="header-body__menu">
-                        <Image className="avatar"
+                        <Image className="header-body__photo"
                                src={avaPic}
                                width={55}
                                height={55}
                         />
                         <Link href="/users">
-                            <a className="link">Профиль</a>
+                            <a className="header-body__profile">Профиль</a>
                         </Link>
                         <Link href="/">
-                            <a className="link">Выход</a>
+                            <a className="header-body__quit">Выход</a>
                         </Link>
                     </div>
                 </div>
             </div>
 
 
-            <div className="wrapper" style={{border: "1px solid gray", borderColor: "gray"}}>
-                <Layout>
-                    <Sider>Sider</Sider>
-                    <Content>Content</Content>
-                </Layout>
-            </div>
+            <div className="main wrapper">
+                <div className="main-body">
+                    <Layout>
+                        <Sider className="main-body__sec1">
+                            <ul className="main-body__list">
+                                <li className="main-body__item">Меню - 1</li>
+                                <li className="main-body__item">Меню - 2</li>
+                                <li className="main-body__item">Меню - 3</li>
+                            </ul>
 
-            {/*<h1 style={{textAlign: "center"}}>Главная страница</h1>*/}
+                        </Sider>
+
+                        <Content className="main-body__sec2">
+                            <Form>
+                                <p>!!!!!!</p>
+                            </Form>
+                        </Content>
+                    </Layout>
+                </div>
+            </div>
 
             <style jsx>
                 {`
                   .logo {
                     width: 114px;
                     height: 55px;
-                  }
-
-                  .link {
-                    text-decoration: none;
-                    color: black;
-                    font-size: 20px;
-                    margin: 10px;
                   }
                 `}
             </style>
