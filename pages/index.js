20843a3aa063baf0d5bbc005f89ca4131b189fae
aleksromanov <kosmonavt-vavilon@mail.ru>
2022-05-17 17:21:56 +0700

    Создание структуры и разметки страницы

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,36 @@
+import Link from "next/link";
+import logoPic from "./krk_logo.svg";
+import Image from 'next/image'
+
 const Index = () => {
     return (
-        <div>
-            <h1>Главная страница</h1>
-            <p>7777111177771111</p>
+        <div className="container">
+            <div className="header">
+                <Image src={logoPic}/>
+                <Link href="/users">
+                    <a className="link">Профиль</a>
+                </Link>
+                <Link href="/">
+                    <a className="link">Выход</a>
+                </Link>
+            </div>
+            <h1 style={{textAlign: "center"}}>Главная страница</h1>
+
+            <style jsx>
+                {`
+                  .link {
+                    text-decoration: none;
+                    color: black;
+                    font-size: 20px;
+                    margin: 10px;
+                  }
+
+                  .logo {
+                    width: 114px;
+                    height: 55px;
+                  }
+                `}
+            </style>
         </div>
     );
 };
---
+++ pages/users.js
@@ -0,0 +1,28 @@
+import {useState} from "react";
+import Link from "next/link";
+
+const Users = () => {
+    const [users, setUsers] = useState([
+        {id: 1, name: 'vasya'},
+        {id: 2, name: 'petya'},
+        {id: 3, name: 'evgeniy'},
+        {id: 4, name: 'aleksey'},
+
+    ])
+    return (
+        <div>
+            <h1>Список пользователей</h1>
+            <ul>
+                {users.map(user =>
+                    <li key={user.id}>
+                        <Link href={`/users/${user.id}`}>
+                            <a>{user.name}</a>
+                        </Link>
+                    </li>
+                )}
+            </ul>
+        </div>
+    );
+};
+
+export default Users;
