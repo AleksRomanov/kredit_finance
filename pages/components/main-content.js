b65a3ca011d016a2a37c4f21dff509178c44c218
aleksromanov <kosmonavt-vavilon@mail.ru>
2022-05-18 12:38:39 +0700

    Создание структуры и разметки страницы ....+ main-content

--- pages/components/main-content.js
+++ pages/components/main-content.js
@@ -1,13 +1,63 @@
 import React from 'react';
 import {Card, Space} from "antd";
+// import 'antd/dist/antd.css';
+import { Cascader } from 'antd';
+
+
+function onChange(value) {
+    console.log(value);
+}
+
+const options = [
+    {
+        value: 'zhejiang',
+        label: 'Zhejiang',
+        children: [
+            {
+                value: 'hangzhou',
+                label: 'Hangzhou',
+                children: [
+                    {
+                        value: 'xihu',
+                        label: 'West Lake',
+                    },
+                ],
+            },
+        ],
+    },
+    {
+        value: 'jiangsu',
+        label: 'Jiangsu',
+        children: [
+            {
+                value: 'nanjing',
+                label: 'Nanjing',
+                children: [
+                    {
+                        value: 'zhonghuamen',
+                        label: 'Zhong Hua Men',
+                    },
+                ],
+            },
+        ],
+    },
+];
 
 const MainContent = () => {
     return (
         <div>
             <Space direction="vertical" size="middle" style={{ display: 'flex' }}>
-                <Card title="Card" size="small">
-                    <p>Card content</p>
-                    <p>Card content</p>
+                <Card size="small">
+                    <Cascader options={options} onChange={onChange} placeholder="Please select" />
+                    <Card style={{ width: 184 }}>
+                        <p>Card content</p>
+                    </Card>
+                    <Card style={{ width: 184 }}>
+                        <p>Card content</p>
+                    </Card>
+                    <Card style={{ width: 184 }}>
+                        <p>Card content</p>
+                    </Card>
                 </Card>
             </Space>
         </div>
